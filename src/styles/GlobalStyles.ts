f1ce4d8c5062f560d3e5d17b82a4af6c3005a17d
frassaolucas <frassao.lucas@gmail.com>
2020-06-22 08:57:30 -0300

    - imported roboto font; - created global styles;

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -6,4 +6,37 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    outline: 0;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  :root {
+    --primary: #36393f;
+    --secondary: #2f3136;
+    --tertiary: rgb(32, 35, 37);
+    --quaternary: #292b2f;
+    --quinary: #393d42;
+    --senary: #828386;
+
+    --white: #FFF;
+    --gray: #8a8c90;
+    --chat-input: rgb(64, 68, 75);
+    --symbol: #74777a;
+
+    --notification: #f84a4b;
+    --discord: #6e86d6;
+    --mention-detail: #f9a839;
+    --mention-message: #413f3f;
+
+    --link: #5d80d6;
+
+    --rocketseat: #6633cc;
+  }
 `;
